c51367b5bac7c535b8907ef4200ddd96b68dec96
ddan42 <121756832+ddan42@users.noreply.github.com>
2023-07-11 10:26:18 +0600

    typos

--- knn_classification.py
+++ knn_classification.py
@@ -3,26 +3,29 @@ import pandas as pd
 
 class MyKNNClf():
     """
-    kNN alogithm's realisation in numpy and pandas.
+    kNN algorithm's realisation with numpy and pandas.
 
-    The relesation of kNN algorthm for classification task which can predict proba 
-    (probability of class 1 in neighbors) and class (mdoe of neighbours). To conut distances, the model can use euclidean, 
-    manhattan, chebyshev or cosine distance. Class prediction based on rank and distance of neighbours are realised too. 
+    The realisation of kNN algorithm for classification task which can predict_proba 
+    (probability of class 1 in neighbors) and class (mode of neighbours). To count distances, 
+    the model can use euclidean, manhattan, chebyshev or cosine distance. 
+    Class prediction based on rank and distance of neighbours are realised too. 
     """
     def __init__(self, k = 3, metric = 'euclidean', weight = 'uniform') -> None:
         """
         Parameters
         ----------
         k : int
-            number of closest neiughbours to choose
+            number of closest neighbours to choose
         
         metric : str
-            One of metrics to calculate the distance between objects: euclidean, manhattan, chebyshev, cosine.
+            One of metrics to calculate the distance between objects: euclidean, 
+            manhattan, chebyshev, cosine.
         
         weights : str
-            One of type of weight to choose when calculating the average of neighbours: uniform (mode), rank (inversely proportional 
-            to the rank of a neighbour based on how close it is), distance (inversely proportional to the distance between test
-            and train row)
+            One of type of weight to choose when calculating the average of neighbours: 
+            uniform (mode), rank (inversely proportional to the rank of a neighbour 
+            based on how close it is), distance (inversely proportional 
+            to the distance between test and train row).
         """
         self.k = k
         self.train_size = None
@@ -33,7 +36,7 @@ class MyKNNClf():
     def __repr__(self) -> str:
         return f'MyKNNClf class: k={self.k}'
     def fit(self, X: pd.DataFrame(dtype = 'object'), y: pd.Series(dtype = 'object')) -> None:
-        """The model just saves the train data. """
+        """The model just saves the train data."""
         self.X = X
         self.y = y
         self.train_size = X.shape
@@ -90,7 +93,8 @@ class MyKNNClf():
             class_weights[0] = reverse_distances[y_k[y_k==0].index].sum()/reverse_distances.sum()
             return class_weights[1]
     def predict(self, X_pred: pd.DataFrame(dtype = 'object')) -> np.array:
-        """Model outputs most popular class or highest probability class inversely proportional to rank or distance of each train row."""
+        """Model outputs most popular class or highest probability 
+        class inversely proportional to rank or distance of each train row."""
         preds = list()
         for idx, row in X_pred.iterrows():
             distances = self.get_distance(row)
@@ -99,7 +103,8 @@ class MyKNNClf():
             preds.append(self.predict_get_pred_class(y_k, distances_k))
         return np.array(preds)
     def predict_proba(self, X_pred: pd.DataFrame()) -> np.array:
-        """Model outputs mean of neighbours or weighted mean of neighbours inveresely proportional to rank or distance of each train row."""
+        """Model outputs mean of neighbours or weighted mean of neighbours 
+        inversely proportional to rank or distance of each train row."""
         preds = list()
         for idx, row in X_pred.iterrows():
             distances = distances = self.get_distance(row)
